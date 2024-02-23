a8d4e5e7adb19f216ebae435f28f38e15654db8b
SamuelPrieur <sprieurpro@gmail.com>
2024-02-23 14:59:14 +0100

    modified:   server.js 	new file:   www/html/index.html

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ let express = require("express");
 let app = express();
 let port = 3000;
 
+app.use(express.static(__dirname + "/www"));
+
 app.listen(port, () => {
   console.log("Server initialised");
   console.log(`Server listening at http://localhost:${port}`);
 });
 
 app.get("/", (req, res, next) => {
-  res.send("Hello World !");
+  res.sendFile("/www/html/index.html");
 });
